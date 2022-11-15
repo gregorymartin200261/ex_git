ed6f7678b71f4ee442e04e3605fb8b1cc92d1a91
gregorymartin <77732347+gregorymartin200261@users.noreply.github.com>
2022-11-15 14:44:31 +0100

    Add files via upload

---
+++ Cercle/Cercle/Cercle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cercle
+{
+    class Cercle
+    {
+        private int _rayon;
+        public Cercle(int rayon)
+        {
+            this._rayon = rayon;
+
+        }
+        public int Rayon
+        {
+            get
+            {
+                return _rayon;
+            }
+            set
+            {
+                _rayon = value;
+            }
+        }
+
+        public double CalculePerimetre()
+        {
+            double value = 2* Math.PI * this._rayon;
+            return value;
+        }
+        public double CalculeAire()
+        {
+            double value = Math.PI * this._rayon * this._rayon;
+            return value;
+        }
+    }
+}
---
+++ Cercle/Cercle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cercle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=0;
+            bool continuer = true;
+
+            while (continuer)
+            {
+                x++;
+                Console.WriteLine("quel est le rayon du cercle?");
+                Cercle rayon = new Cercle(int.Parse(Console.ReadLine()));
+                Console.Clear();
+
+                double Air = rayon.CalculeAire();
+                double Perimetre = rayon.CalculePerimetre();
+                //double Phrase = rayon.AfficheCaracteristique
+            }
+
+        }
+
+    }
+}
+
