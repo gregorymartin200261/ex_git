0d8c5918465b9c2504551ec39870934a9a75ca84
gregorymartin <77732347+gregorymartin200261@users.noreply.github.com>
2022-12-04 15:00:27 +0100

    Add files via upload

---
+++ nombre_complexe/nombre_complexe/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace nombre_complexe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Continuer = true;
+
+            do
+            {
+                Console.Clear();
+                Complexe[] Complexe = new Complexe[2];
+                int i = 0;
+                Console.WriteLine("quel est le reel n°1 du complexe de depart?");
+                double reel1 = (int.Parse(Console.ReadLine()));
+                Console.WriteLine("quel est le reel n°2 du complexe de depart?");
+                double reel2 = (int.Parse(Console.ReadLine()));
+
+                Complexe[i] = new Complexe(reel1, reel2);
+                double module = Complexe[0].CalculeModule();
+
+                Console.WriteLine(Complexe[i].AfficheComplexe(module));
+
+                i++;
+
+                Console.WriteLine("quel est le reel n°1 du second complexe?");
+                reel1 = (int.Parse(Console.ReadLine()));
+                Console.WriteLine("quel est le reel n°2 du second complexe?");
+                reel2 = (int.Parse(Console.ReadLine()));
+
+                Complexe[i] = new Complexe(reel1, reel2);
+
+                double reel1Ajoute=Complexe[1].reel1;
+                double ajouteFinal1 = Complexe[0].ajoute1(reel1Ajoute);
+
+                double reel2Ajoute = Complexe[1].reel2;
+                double ajouteFinal2 = Complexe[0].ajoute2(reel2Ajoute);
+
+                Console.WriteLine("le complexe de base dedvient : ("+ajouteFinal1 + ","+ ajouteFinal2+")");
+                Console.ReadLine();
+                Console.WriteLine("Voulez vous recommencer?(oui/non)");
+                string Recom = Console.ReadLine();
+                if (Recom == "non")
+                {
+                    Continuer = false;
+                }
+            }
+            while (Continuer);
+        }
+    }
+}
---
+++ nombre_complexe/nombre_complexe/function.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nombre_complexe
+{
+    class Complexe
+    {
+        private double _reel1;
+        private double _reel2;
+        public Complexe(double reel1, double reel2)
+        {
+            this._reel1 = reel1;
+            this._reel2 = reel2;
+
+        }
+
+        public double reel1
+        {
+            get
+            {
+                return this._reel1;
+            }
+            set
+            {
+                this._reel1 = value;
+            }
+        }
+        public double reel2
+        {
+            get
+            {
+                return this._reel2;
+            }
+            set
+            {
+                this._reel2 = value;
+            }
+        }
+
+        public double ajoute1(double reel1Ajoute) 
+        {
+            double Value = reel1 + reel1Ajoute;
+            reel1 = Value;
+            return reel1;
+        }
+        public double ajoute2(double reel2Ajoute)
+        {
+            double Value = reel2 + reel2Ajoute;
+            reel2 = Value;
+            return reel1;
+        }
+        public double CalculeModule()
+        {
+            double value = Math.Sqrt( this._reel1 * this._reel1 + this._reel2 * this._reel2);
+            return value;
+        }
+
+        public string AfficheComplexe(double module)
+        {
+            return "le premier complexe : (" + this._reel1 + ",00;" + this._reel2 + ",00) a pour module : " + module;
+        }
+
+    }
+}
