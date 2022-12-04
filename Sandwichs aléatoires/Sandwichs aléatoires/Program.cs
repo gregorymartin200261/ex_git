d12a8b051ca89b51f2e108b1918e89f5c0b0239e
gregorymartin <77732347+gregorymartin200261@users.noreply.github.com>
2022-12-04 14:59:21 +0100

    Add files via upload

---
+++ Sandwichs aléatoires/Sandwichs aléatoires/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sandwichs_aléatoires
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Continuer = true;
+
+            do
+            {
+                Console.Clear();
+                SandwichMaker SandwichMaker = new SandwichMaker();
+                Console.WriteLine("Votre sandwish contient: " + SandwichMaker.RndmProteine() +" ; "+ SandwichMaker.RndmCondiments() + " ; " + SandwichMaker.RndmPain() + " ; " + SandwichMaker.RndmCrudites());
+                Console.WriteLine("Voulez vous recommencer?(oui/non)");
+                string Recom = Console.ReadLine();
+                if (Recom == "non")
+                {
+                    Continuer = false;
+                }
+            }
+            while (Continuer);
+        }
+    }
+}
---
+++ Sandwichs aléatoires/Sandwichs aléatoires/SandwichMaker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sandwichs_aléatoires
+{
+    class SandwichMaker
+    {
+        string[] _proteine = { "jambon", "fromage", "roast beef", "salami" };
+        string[] _condiments = { "moutarde", "guacamole", "mayo", "Purée d'haricots noirs" };
+        string[] _pain = { "pain de campagne", "pain complet", "pain de seigle", "pains spéciaux" };
+        string[] _crudites = { "salades", "tomates", "carotes", "oignons" };
+
+        public string RndmProteine()
+        {
+            Random rnd = new Random();
+            int min = 0;
+            int max = 4;
+            return _proteine[rnd.Next(min, max)];
+        }
+        public string RndmCondiments()
+        {
+            Random rnd = new Random();
+            int min = 0;
+            int max = 4;
+            return _condiments[rnd.Next(min, max)];
+        }
+        public string RndmPain()
+        {
+            Random rnd = new Random();
+            int min = 0;
+            int max = 4;
+            return _pain[rnd.Next(min, max)];
+        }
+        public string RndmCrudites()
+        {
+            Random rnd = new Random();
+            int min = 0;
+            int max = 4;
+            return _crudites[rnd.Next(min, max)];
+        }
+    }
+}
