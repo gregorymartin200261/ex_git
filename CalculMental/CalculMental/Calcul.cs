2e4f8113a1b8d213ae32b14f38e43475d0ff6464
gregorymartin <77732347+gregorymartin200261@users.noreply.github.com>
2022-12-04 22:43:55 +0100

    Add files via upload

---
+++ CalculMental/CalculMental/Calcul.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculMental
+{
+    class Calcul
+    {
+        private string _op;
+        private int _n1;
+        private int _n2;
+
+        public string Op
+        {
+            get
+            {
+                return _op;
+            }
+        }
+        public int N1
+        {
+            get
+            {
+                return _n1;
+            }
+        }
+        public int N2
+        {
+            get
+            {
+                return _n2;
+            }
+        }
+        // constructeur
+        public Calcul(bool addition)
+        {
+            Random r = new Random();
+            if (addition)
+            {
+                _op = "+";
+            }
+            else
+            {
+                _op = "*";
+            }
+            _n1 = r.Next(1, 10);
+            _n2 = r.Next(1, 10);
+        }
+        // vérifie si une réponse est correcte
+        public bool VerifOpe(int rep)
+        {
+            bool verif;
+            if (_op == "+")
+            {
+                verif = (rep == _n1 +_n2 );
+            }
+            else
+            {
+                verif = (rep == _n1 * _n2);
+            }
+            return verif;
+        }
+    }
+
+
+
+}
---
+++ CalculMental/CalculMental/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CalculMental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random alea = new Random();
+            Console.WriteLine("Bienvenue dans ce petit programme de calcul mental.");
+            Calcul r = new Calcul((int)alea.Next(2) == 1);
+            while (true)
+            {
+                Console.Write($"{r.N1}{r.Op}{r.N2} = ");
+                string rep = Console.ReadLine();
+                int repEntiere;
+                // mauvaise entrée utilisateur
+                if (!int.TryParse(rep, out repEntiere))
+                {
+                    Console.WriteLine("Merci d'avoir joué");
+                    return;
+                }
+                // vérification de la réponse
+                else if (r.VerifOpe(repEntiere))
+                {
+                    Console.WriteLine("Correct !");
+                    r = new  Calcul((int)alea.Next(2) == 1);
+                }
+                // mauvaise réponse au calcul
+                else
+                {
+                    Console.WriteLine("Erreur, recommencez !");
+                }
+            }
+        }
+    }
+}
