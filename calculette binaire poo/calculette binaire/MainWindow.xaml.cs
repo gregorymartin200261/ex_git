b0b1c10147c91e3cde9c9868593981e36e5a5741
gregorymartin <77732347+gregorymartin200261@users.noreply.github.com>
2023-01-18 23:22:40 +0100

    Add files via upload

---
+++ calculette binaire poo/calculette binaire/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculette_binaire
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculer_Click(object sender, RoutedEventArgs e)
+        {
+            calculatrice calcul = new calculatrice();
+
+            ushort[] t1 =calcul.RemplirTableau(Nbr1.Text);
+            ushort[] t2 =calcul.RemplirTableau(Nbr2.Text);
+
+            calcul = new calculatrice(t1, t2);
+
+            bool ok=false;
+            ushort[] tRes = new ushort[8];
+
+            if (RadAddition.IsChecked==true ||RadSoustraction.IsChecked==true )
+            {
+                if (RadAddition.IsChecked==true)
+                {
+                    ok = calcul.Additionne(ref tRes);
+                }
+                else if(RadSoustraction.IsChecked == true)
+                {
+                    ok = calcul.Soustraire(ref tRes);
+                }
+                if (ok)
+                {
+
+                    Result.Text = calcul.check(tRes);
+                    
+                }
+                
+            }
+
+            
+        }
+    }
+}
---
+++ calculette binaire poo/calculette binaire/calculatrice.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculette_binaire
+{
+    class calculatrice
+    {
+        private ushort[] _t1 = new ushort[8];
+        private ushort[] _t2 = new ushort[8];
+
+        public calculatrice(ushort[] t1, ushort[] t2)
+        {
+            this._t1 = t1;
+            this._t2 = t2;
+        }
+
+        static string check(ref ushort[] tRes)
+        {
+            string Resultat = "";
+            for (int i = 0; i < tRes.Length; i++)
+            {
+                Resultat = Resultat + tRes[i].ToString();
+            }
+            return Resultat;
+        }
+
+        public ushort[] RemplirTableau(string nombreBinaire)
+        {
+            ushort[] tabBin = new ushort[8];
+            for (int i = 0; i < 8; i++)
+            {
+                tabBin[i] = 0;
+            }
+            for (int i = 0; i <= nombreBinaire.Length-1; i++)
+            {
+                tabBin[7 - i] = ushort.Parse(nombreBinaire[nombreBinaire.Length - 1 - i].ToString());
+            }
+            return tabBin;
+        }
+
+        static bool Additionne(ref ushort[] tRes)
+        {
+            bool ok = true;
+            ushort report = 0;
+            ushort Res;
+
+            for (int i = 0; i < 8; i++)
+            {
+                Res = (ushort)(_t1[i] + _t2[i] + report);
+                if (Res/2 ==0)
+                {
+                    report = 0;
+                }
+                else { report = 1; }
+                if (Res==1)
+                {
+                    tRes[i] = 1;
+                }
+                else
+                {
+                    if (Res % 2==1)
+                    {
+                        tRes[i] = 1;
+                    }
+                    else
+                    {
+                        tRes[i] = 0;
+                    }
+                }
+            }
+            if (report == 1)
+            {
+                ok = false;
+            }
+            return ok;
+        }
+
+        public bool Soustraire(ref ushort[] tRes)
+        {
+            tRes = new ushort[8];
+            int[] tTemp = new int[8];
+            bool ok = true;
+
+            for (int i = 0; i <= 7; i++)
+            {
+                tTemp[i] = _t1[i] - _t2[i];
+            }
+
+            for (int i = 7; i >= 1; i--)
+            {
+                if (tTemp[i]==-1)
+                {
+                    _t2[i - 1] =(ushort)(_t2[i - 1] + 1);
+                    _t1[i] =(ushort)( _t1[i] + 2);
+                }
+                if (_t1[i]<_t2[i])
+                {
+                    _t2[i - 1] = (ushort)(_t2[i - 1] + 1);
+                    _t1[i] = (ushort)(_t1[i] + 2);
+                    
+                }
+                tRes[i] = (ushort)(_t1[i] - _t2[i]);
+            }
+            if (_t1[0]>=_t2[0])
+            {
+                tRes[0] = (ushort)(_t1[0] - _t2[0]);
+            }
+            else { ok = false; }
+            return ok;
+        
+        }
+    }
+}
